046809322d89fe452c332f6a06e2fa922ba80698
nelu572 <s25031@gsm.hs.kr>
2026-03-04 11:44:53 +0900

    카메라 위치가 마우스 거리에 맞게 바뀜

--- Assets/Script/Game/PlayerLookMouse.cs
+++ Assets/Script/Game/PlayerLookMouse.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerLookMouse : MonoBehaviour
 {
+    [SerializeField] private GameObject camPoint;
+    private Vector3 camPos = new Vector3();
 
     [SerializeField] private Camera cam;
 
@@ -9,7 +11,7 @@ public class PlayerLookMouse : MonoBehaviour
     {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
 
         if (groundPlane.Raycast(ray, out float distance))
         {
@@ -17,10 +19,22 @@ public class PlayerLookMouse : MonoBehaviour
             Vector3 direction = targetPoint - transform.position;
             direction.y = 0f;
 
+            Vector3 offset = targetPoint - transform.position;
+            offset.y = 0f;
+
+            float maxDistance = 10f;
+            offset = Vector3.ClampMagnitude(offset, maxDistance);
+
+            camPos = transform.position + offset * 0.3f;
+
             if (direction != Vector3.zero)
             {
                 transform.rotation = Quaternion.LookRotation(direction);
             }
         }
+    }
+    void LateUpdate()
+    {
+        camPoint.transform.position = camPos;
     }
 }
